b2be661f2af4e7802d56864ff881513119a8f665
prakashrrmch <prakashfime@gmail.com>
2022-10-20 19:08:30 +0530

    added a new function sum

--- data_processing.py
+++ data_processing.py
@@ -1,1 +1,9 @@
-print("adding all the data processing code related to fraud detection")
+print("adding all the data processing code related to staff leaves")
+
+a = 5
+b = 7
+def sum(a,b):
+    c = a+b
+    return c
+
+print("added a new function sum")
